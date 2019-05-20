e2c1579f51f4fccc3db76011481231a365733b7e
Fanny CATUSSE <fcatusse@e1r6p3.42.fr>
2019-05-20 19:19:24 +0200

    add day2

--- Day01/ex05/epur_str.php
+++ Day01/ex05/epur_str.php
@@ -2,7 +2,7 @@
 <?php
     if ($argc == 2)
     {
-        $str = preg_replace('/\s\s+/',' ', $argv[1]);
+        $str = preg_replace('/\s+/',' ', $argv[1]);
         $str = trim($str);
         echo "$str\n";
     }
