74105bba7ac13c92df35054f21ed84f7f7312831
fanny <fannykatusse@gmail.com>
2019-05-21 22:48:54 +0200

    starter exo photos

--- Day02/ex04/photos.php
+++ Day02/ex04/photos.php
@@ -1,4 +1,11 @@
 #!/usr/bin/php
 <?php
-    
+    $html = file_get_contents($argv[1]);
+    preg_match_all('/<img[^>]+>/i', $html, $img);
+    print_r($img);
+    for ($i = 0; $i < count($img[0]); $i++)
+    {
+        preg_match_all('/src="([^"]+)/i', $img[0][$i], $img_src);
+    }
+    print_r($img_src);
 ?>
